d7278bae16fc9f2f4d527e732fcf363223cc58bf
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-02-10 23:13:33 -0500

    Added Operate function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let num1, num2 = 0; 
+let operator = '';
+
 const add = (a,b) => a+b; 
 
 const sub = (a,b) => a-b; 
@@ -6,3 +9,15 @@ const mult = (a,b) => a*b;
 
 const div = (a,b) => (b !== 0 ? a / b : "Error: Division by zero");; 
 
+function operate(operator, num1, num2) {
+    switch(operator){
+        case '+':
+            add(num1, num2); 
+        case '-':
+            sub(num1, num2); 
+        case '*':
+            mult(num1, num2); 
+        case '/': 
+            div(num1, num2); 
+    }
+}
